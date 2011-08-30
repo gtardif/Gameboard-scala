cb187348f33f0a0fe56b7587ecc81842c7b6ced4
Guillaume Tardif <guillaume.tardif@algodeal.com>
2011-08-30 11:34:04 +0200

    Scala specs with JUnit

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ libraryDependencies ++= Seq(
 	"org.scala-lang" % "scala-swing" % "2.9.0-1",
 //	"org.scala-tools.testing" %% "scalacheck" % "1.9" % "test",
 	"org.scala-tools.testing" %% "specs" % "1.6.8" % "test", 
-	"org.mockito" % "mockito-core" % "1.9.0-rc1" % "test"
+	"org.mockito" % "mockito-core" % "1.9.0-rc1" % "test",
+	"junit" % "junit" % "4.7" % "test"
 	 )
--- src/test/scala/gameboard/P4BoardTest.scala
+++ src/test/scala/gameboard/P4BoardTest.scala
@@ -1,19 +1,30 @@
- import org.specs.Specification
-  import org.specs.mock.Mockito
+package gameboard
 
-  object spec extends Specification with Mockito {
-    
+import org.specs.SpecificationWithJUnit
+import org.specs.mock.Mockito
+import org.specs.runner.JUnit
+
+class P4BpardTest extends SpecificationWithJUnit with Mockito {
+  
+   "'hello world' has 11 characters" in {
+     "hello world".size must_== 11
+  }
+  
+  "mockito" in {
     val m = mock[java.util.List[String]] // a concrete class would be mocked with: mock(new java.util.LinkedList[String])
-    
+
     // stub a method call with a return value
     m.get(0) returns "one"
-  
+
     // call the method
     m.get(0)
-  
+
     // verify that the call happened, this is an expectation which will throw a FailureException if that is not the case
     there was one(m).get(0)
-    
+
     // we can also check that another call did not occur
     there was no(m).get(1)
+
   }
+
+}
