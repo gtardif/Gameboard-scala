8b1c6ce62b7f40a0a8bd9da8a010b46c2a47f9b9
Guillaume Tardif <guillaume.tardif@algodeal.com>
2011-08-30 18:10:42 +0200

    started Bot with pattern

--- src/main/scala/gameboard/P4Bot.scala
+++ src/main/scala/gameboard/P4Bot.scala
@@ -1,10 +1,27 @@
 package gameboard
 
+import Side._
+
 class P4Bot(val side: Side.Side) extends Player {
   def updateBoard(board: P4Board, game: P4Game) {
-    if (board.player == side) {
-      game.play(3, this)
+    if (board.player != side) {
+      return
     }
+
+    val nbRed = (board.columns).map {
+      _ match {
+        case (YELLOW :: YELLOW :: YELLOW :: _) => 3
+        case (_) => 0
+      }
+    }
+
+    val colsWithNbReds = (nbRed zipWithIndex) groupBy (_._1)
+    if (colsWithNbReds.contains(3)) {
+      val colsWith3Red = colsWithNbReds(3)
+      game.play(colsWith3Red(0)._2, this)
+    }
+
+    game.play(3, this)
   }
 
   def newMessage(message: String) {
--- src/test/scala/gameboard/P4BotTest.scala
+++ src/test/scala/gameboard/P4BotTest.scala
@@ -7,25 +7,34 @@ import org.specs.SpecificationWithJUnit
 
 class P4BotTest extends SpecificationWithJUnit with Mockito {
   val mockBoard = mock[P4Board]
+  mockBoard.player returns RED
+  mockBoard.columns returns List()
   val mockGame = mock[P4Game]
 
   val bot = new P4Bot(RED)
 
   "P4 bot" should {
     "start game in middle column" in {
-      mockBoard.player returns RED
 
       bot.updateBoard(mockBoard, mockGame)
 
       there was one(mockGame).play(3, bot)
     }
-    
+
     "play only when it is its turn" in {
       mockBoard.player returns YELLOW
-      
+
       bot.updateBoard(mockBoard, mockGame)
 
       there was no(mockGame).play(3, bot)
+    }
+
+    "block columns where opponent can win" in {
+      val board = new P4Board(List(List(), List(),List(), List(RED, RED), List(YELLOW, YELLOW, YELLOW), List(), List()), RED)
+      
+      bot.updateBoard(board, mockGame)
+      
+      there was one(mockGame).play(4, bot)
     }
   }
 }
